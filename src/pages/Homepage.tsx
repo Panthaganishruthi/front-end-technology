0531ad6df6119ef7297a454242b07dcb7ced4f78
Panthaganishruthi <142817173+Panthaganishruthi@users.noreply.github.com>
2023-10-18 22:35:39 +0530

    Add files via upload
    front-end

---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,312 @@
+import { FunctionComponent } from "react";
+import styles from "./Homepage.module.css";
+
+const Homepage: FunctionComponent = () => {
+  return (
+    <div className={styles.homepage}>
+      <div className={styles.herroBanner}>
+        <div className={styles.herroBannerChild} />
+        <img
+          className={styles.goodHumoredWomanHoldsDogLIcon}
+          alt=""
+          src="/goodhumoredwomanholdsdoglaughingpinkbackgroundemotionalsorthairedgirlgreyhoodieposeswithcorgiisolated-1@2x.png"
+        />
+        <div className={styles.oneMoreFriend}>One more friend</div>
+        <b className={styles.thousandsMoreFun}>Thousands more fun!</b>
+        <div className={styles.havingAPet}>
+          Having a pet means you have more joy, a new friend, a happy person who
+          will always be with you to have fun. We have 200+ different pets that
+          can meet your needs!
+        </div>
+        <div className={styles.buttonL}>
+          <div className={styles.clickHere}>Explore Now</div>
+        </div>
+        <div className={styles.buttonL1}>
+          <div className={styles.clickHere}>View Intro</div>
+          <img
+            className={styles.mediaPlayCircle}
+            alt=""
+            src="/media--play-circle.svg"
+          />
+        </div>
+      </div>
+      <div className={styles.whatsNewParent}>
+        <div className={styles.clickHere}>Whats new?</div>
+        <b className={styles.takeALook}>Take a look at some of our pets</b>
+      </div>
+      <div className={styles.productCard}>
+        <div className={styles.image2Wrapper}>
+          <div className={styles.image2} />
+        </div>
+        <div className={styles.productCardInner}>
+          <div className={styles.mo231PomeranianWhiteParent}>
+            <b className={styles.mo231Pomeranian}>MO231 - Pomeranian White</b>
+            <div className={styles.frameParent}>
+              <div className={styles.geneParent}>
+                <div className={styles.gene}>Gene:</div>
+                <b className={styles.male}>Male</b>
+              </div>
+              <div className={styles.gene} />
+              <div className={styles.geneParent}>
+                <div className={styles.gene}>Age:</div>
+                <b className={styles.male}>02 months</b>
+              </div>
+            </div>
+            <b className={styles.vnd}>6.900.000 VND</b>
+          </div>
+        </div>
+      </div>
+      <div className={styles.productCard1}>
+        <div className={styles.image2Container}>
+          <div className={styles.image2} />
+        </div>
+        <div className={styles.productCardInner}>
+          <div className={styles.mo231PomeranianWhiteParent}>
+            <b className={styles.mo231Pomeranian}>MO231 - Pomeranian White</b>
+            <div className={styles.frameParent}>
+              <div className={styles.geneParent}>
+                <div className={styles.gene}>Gene:</div>
+                <b className={styles.male}>Male</b>
+              </div>
+              <div className={styles.gene} />
+              <div className={styles.geneParent}>
+                <div className={styles.gene}>Age:</div>
+                <b className={styles.male}>02 months</b>
+              </div>
+            </div>
+            <b className={styles.vnd}>6.900.000 VND</b>
+          </div>
+        </div>
+      </div>
+      <div className={styles.productCard2}>
+        <div className={styles.image2Container}>
+          <div className={styles.image2} />
+        </div>
+        <div className={styles.productCardInner}>
+          <div className={styles.mo231PomeranianWhiteParent}>
+            <b className={styles.mo231Pomeranian}>MO502 - Poodle Tiny Yellow</b>
+            <div className={styles.frameParent}>
+              <div className={styles.geneParent}>
+                <div className={styles.gene}>Gene:</div>
+                <b className={styles.male}>Female</b>
+              </div>
+              <div className={styles.gene} />
+              <div className={styles.geneParent}>
+                <div className={styles.gene}>Age:</div>
+                <b className={styles.male}>02 months</b>
+              </div>
+            </div>
+            <b className={styles.vnd}>3.900.000 VND</b>
+          </div>
+        </div>
+      </div>
+      <div className={styles.productCard3}>
+        <div className={styles.image2Wrapper1}>
+          <div className={styles.image23} />
+        </div>
+        <div className={styles.productCardInner}>
+          <div className={styles.mo231PomeranianWhiteParent}>
+            <b className={styles.mo231Pomeranian}>MO502 - Poodle Tiny Yellow</b>
+            <div className={styles.frameParent}>
+              <div className={styles.geneParent}>
+                <div className={styles.gene}>Gene:</div>
+                <b className={styles.male}>Female</b>
+              </div>
+              <div className={styles.gene} />
+              <div className={styles.geneParent}>
+                <div className={styles.gene}>Age:</div>
+                <b className={styles.male}>02 months</b>
+              </div>
+            </div>
+            <b className={styles.vnd}>3.900.000 VND</b>
+          </div>
+        </div>
+      </div>
+      <div className={styles.image24} />
+      <div className={styles.productCard4}>
+        <div className={styles.image2Container}>
+          <div className={styles.image2} />
+        </div>
+        <div className={styles.productCardInner}>
+          <div className={styles.mo231PomeranianWhiteParent}>
+            <b className={styles.mo231Pomeranian}>MO102 - Poodle Tiny Sepia</b>
+            <div className={styles.frameParent}>
+              <div className={styles.geneParent}>
+                <div className={styles.gene}>Gene:</div>
+                <b className={styles.male}>Male</b>
+              </div>
+              <div className={styles.gene} />
+              <div className={styles.geneParent}>
+                <div className={styles.gene}>Age:</div>
+                <b className={styles.male}>02 months</b>
+              </div>
+            </div>
+            <b className={styles.vnd}>4.000.000 VND</b>
+          </div>
+        </div>
+      </div>
+      <div className={styles.productCard5}>
+        <div className={styles.image2Wrapper1}>
+          <div className={styles.image23} />
+        </div>
+        <div className={styles.productCardInner}>
+          <div className={styles.mo231PomeranianWhiteParent}>
+            <b className={styles.mo231Pomeranian}>MO102 - Poodle Tiny Sepia</b>
+            <div className={styles.frameParent}>
+              <div className={styles.geneParent}>
+                <div className={styles.gene}>Gene:</div>
+                <b className={styles.male}>Male</b>
+              </div>
+              <div className={styles.gene} />
+              <div className={styles.geneParent}>
+                <div className={styles.gene}>Age:</div>
+                <b className={styles.male}>02 months</b>
+              </div>
+            </div>
+            <b className={styles.vnd}>4.000.000 VND</b>
+          </div>
+        </div>
+      </div>
+      <div className={styles.image3} />
+      <div className={styles.productCard6}>
+        <div className={styles.image2Container}>
+          <div className={styles.image2} />
+        </div>
+        <div className={styles.productCardInner}>
+          <div className={styles.mo231PomeranianWhiteParent}>
+            <b
+              className={styles.mo231Pomeranian}
+            >{`MO512 - Alaskan Malamute `}</b>
+            <div className={styles.frameParent}>
+              <div className={styles.geneParent}>
+                <div className={styles.gene}>Gene:</div>
+                <b className={styles.male}>Male</b>
+              </div>
+              <div className={styles.gene} />
+              <div className={styles.geneParent}>
+                <div className={styles.gene}>Age:</div>
+                <b className={styles.male}>02 months</b>
+              </div>
+            </div>
+            <b className={styles.vnd}>8.900.000 VND</b>
+          </div>
+        </div>
+      </div>
+      <div className={styles.productCard7}>
+        <div className={styles.image2Wrapper1}>
+          <div className={styles.image23} />
+        </div>
+        <div className={styles.productCardInner}>
+          <div className={styles.mo231PomeranianWhiteParent}>
+            <b className={styles.mo231Pomeranian}>MO512 - Alaskan Malamute</b>
+            <div className={styles.frameParent}>
+              <div className={styles.geneParent}>
+                <div className={styles.gene}>Gene:</div>
+                <b className={styles.male}>Male</b>
+              </div>
+              <div className={styles.gene} />
+              <div className={styles.geneParent}>
+                <div className={styles.gene}>Age:</div>
+                <b className={styles.male}>02 months</b>
+              </div>
+            </div>
+            <b className={styles.vnd}>8.900.000 VND</b>
+          </div>
+        </div>
+      </div>
+      <div className={styles.buttonMParent}>
+        <div className={styles.buttonM}>
+          <div className={styles.clickHere2}>All</div>
+          <img
+            className={styles.arrowChevronRightMd}
+            alt=""
+            src="/arrow--chevron-right-md.svg"
+          />
+        </div>
+        <div className={styles.buttonM1}>
+          <div className={styles.clickHere2}>Dogs</div>
+          <img
+            className={styles.arrowChevronRightMd}
+            alt=""
+            src="/arrow--chevron-right-md.svg"
+          />
+        </div>
+        <div className={styles.buttonM1}>
+          <div className={styles.clickHere2}>Cats</div>
+          <img
+            className={styles.arrowChevronRightMd}
+            alt=""
+            src="/arrow--chevron-right-md.svg"
+          />
+        </div>
+      </div>
+      <div className={styles.footer}>
+        <div className={styles.monitoAllRightsReservedParent}>
+          <div className={styles.monitoAllRights}>
+            © 2022 Monito. All rights reserved.
+          </div>
+          <div className={styles.termsOfService}>
+            Terms of Service Privacy Policy
+          </div>
+          <img className={styles.frameIcon} alt="" src="/frame.svg" />
+        </div>
+        <div className={styles.frameParent6}>
+          <div className={styles.registerNowSoYouDontMissParent}>
+            <b className={styles.registerNowSo}>
+              Register now so you don't miss our programs
+            </b>
+            <div className={styles.inputParent}>
+              <div className={styles.input}>
+                <div className={styles.typeSomethingHere}>Enter your Email</div>
+              </div>
+              <div className={styles.buttonL2}>
+                <div className={styles.clickHere}>Subcribe Now</div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.frameParent7}>
+            <div className={styles.homeParent}>
+              <div className={styles.clickHere}>Home</div>
+              <div className={styles.clickHere}>Category</div>
+              <div className={styles.clickHere}>About</div>
+              <div className={styles.clickHere}>Contact</div>
+            </div>
+            <div className={styles.facebookNegativeParent}>
+              <img
+                className={styles.mediaPlayCircle}
+                alt=""
+                src="/facebook--negative.svg"
+              />
+              <img
+                className={styles.mediaPlayCircle}
+                alt=""
+                src="/twitter--negative.svg"
+              />
+              <img
+                className={styles.mediaPlayCircle}
+                alt=""
+                src="/instagram--negative1.svg"
+              />
+              <img
+                className={styles.mediaPlayCircle}
+                alt=""
+                src="/youtube--negative1.svg"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <img className={styles.dog31Icon} alt="" src="/dog3-1@2x.png" />
+      <img className={styles.dog21Icon} alt="" src="/dog2-1@2x.png" />
+      <img className={styles.dog41Icon} alt="" src="/dog4-1@2x.png" />
+      <img className={styles.dog51Icon} alt="" src="/dog5-1@2x.png" />
+      <img className={styles.cat31Icon} alt="" src="/cat3-1@2x.png" />
+      <img className={styles.cat41Icon} alt="" src="/cat4-1@2x.png" />
+      <img className={styles.cat61Icon} alt="" src="/cat6-1@2x.png" />
+      <div className={styles.cat62} />
+      <img className={styles.cat51Icon} alt="" src="/cat5-1@2x.png" />
+    </div>
+  );
+};
+
+export default Homepage;
